b74a2462ffb74852b797e7a5d249849cbaf3ac4a
mustafakba <kaba.mustafaa@gmail.com>
2022-03-29 21:43:05 +0300

    categories-page edited

---
+++ src/components/cards/CategoryCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="category-card text-white rounded"
+    :style="'background:' + info.color"
+  >
+    <div>BookIcon</div>
+    <h3 class="category-card-text">{{ info.text }}</h3>
+  </div>
+</template>
+
+<script>
+import BookIcon from "../icons/BookIcon.vue";
+export default {
+  name: "CategoryCard",
+  props: {
+    info: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.category-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 210px;
+  min-height: 135px;
+  filter: drop-shadow(0px 3px 15px rgba(0, 0, 0, 0.1));
+
+  &-text {
+    font-size: 1rem;
+  }
+}
+</style>
--- src/pages/CategoriesPage.vue
+++ src/pages/CategoriesPage.vue
@@ -1,10 +1,46 @@
 <template>
-  <div>KATEGORİ SAYFASI</div>
+  <div id="category" class="py-5">
+    <div class="container-app">
+      <div>
+        <div class="row">
+          <div
+            v-for="(item, index) in cards"
+            :key="index"
+            class="col-6 col-md-3 mb-4"
+          >
+            <category-card :info="item" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import CategoryCard from "../components/cards/CategoryCard.vue";
+
 export default {
   name: "CategoriesPage",
+  components: {
+    CategoryCard,
+  },
+  setup() {
+    const cards = ref([
+      { color: "#FFFFFF80", svg: 55, text: "Kategori Ekle" },
+      { color: "#F8BD2380", svg: 7, text: "Tarih" },
+      { color: "#81C5D880", svg: 20, text: "Felsefe" },
+      { color: "#2DCE8980", svg: 10, text: "Bilim" },
+      { color: "#59459380", svg: 15, text: "Fantastik" },
+      { color: "#FF7B1280", svg: 7, text: "Edebiyat" },
+      { color: "#94EC1B80", svg: 20, text: "Roman" },
+      { color: "#9EC12F80", svg: 10, text: "Biyografi" },
+    ]);
+
+    return {
+      cards,
+    };
+  },
 };
 </script>
 
