6baee2042cd99ce3234dc0321f13eaae09a81bce
mustafakba <kaba.mustafaa@gmail.com>
2022-03-30 14:33:12 +0300

    bookpage & bookcard edited

---
+++ src/components/cards/BookCard.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="book-card">
+    <div class="book-card-img">
+      <img src="/img/bookimage.png" alt="" />
+    </div>
+    <div class="book-info">
+      <div class="book-info-row">
+        <div class="book-info-row-title">
+          <h5>Kitap Adı</h5>
+          <span>:</span>
+        </div>
+        <div class="book-info-row-desc">
+          <span>Osmanlı Dönemi</span>
+        </div>
+      </div>
+      <div class="book-info-row">
+        <div class="book-info-row-title">
+          <h5>Yazarı</h5>
+          <span>:</span>
+        </div>
+        <div class="book-info-row-desc">
+          <span>İlber</span>
+        </div>
+      </div>
+      <div class="book-info-row">
+        <div class="book-info-row-title">
+          <h5>Yayınevi</h5>
+          <span>:</span>
+        </div>
+        <div class="book-info-row-desc">
+          <span>Can</span>
+        </div>
+      </div>
+      <div class="book-info-row">
+        <div class="book-info-row-title">
+          <h5>Kit</h5>
+          <span>:</span>
+        </div>
+        <div class="book-info-row-desc">
+          <span>Osmanlı</span>
+        </div>
+      </div>
+      <div class="book-info-row">
+        <div class="book-info-row-title">
+          <h5>Kitap Adı</h5>
+          <span>:</span>
+        </div>
+        <div class="book-info-row-desc">
+          <span>Osmanlı Dönemi</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookCard",
+};
+</script>
+
+<style lang="scss" scoped>
+.book-card {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem;
+  background-color: #fff;
+  max-width: 450px;
+  max-height: 200px;
+
+  &-img {
+    margin-right: 2rem;
+  }
+
+  .book-info {
+    width: 100%;
+
+    span {
+      display: flex;
+      font-size: 14px;
+      font-weight: 400;
+      color: #787878;
+      align-items: center;
+      justify-content: center;
+      text-align: right;
+    }
+
+    h5 {
+      min-width: 25px;
+      font-size: 14px;
+      line-height: 21px;
+      font-weight: bold;
+      color: #787878;
+      margin: 0;
+    }
+
+    &-row {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0.2rem 0;
+
+      &-title {
+        max-width: 75px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-right: 1rem;
+      }
+
+      &-desc {
+        white-space: nowrap;
+        width: 100%;
+        display: flex;
+        justify-content: start;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/BookPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="category" class="py-5">
+    <div class="container-app">
+      <div>
+        <div class="row">
+          <div v-for="i in 4" :key="i" class="col-12 col-md-6 mb-4">
+            <book-card />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import BookCard from "../components/cards/BookCard.vue";
+
+export default {
+  name: "BookPage",
+  components: {
+    BookCard,
+  },
+};
+</script>
+
+<style>
+
+</style>
